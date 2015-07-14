22f1410189eb84caad7f508c3cdaeef98230a06d
Mateusz B. <DraconisBoL@users.noreply.github.com>
2015-07-14 14:19:32 +0200

    Update Corki - Daring Bombardier.lua

--- Corki - Daring Bombardier.lua
+++ Corki - Daring Bombardier.lua
@@ -376,6 +376,81 @@ function Variables()
 	_G.oldDrawCircle = rawget(_G, 'DrawCircle')
 	_G.DrawCircle = DrawCircle2	
 	
+	ItemNames				= {
+		[3303]				= "ArchAngelsDummySpell",
+		[3007]				= "ArchAngelsDummySpell",
+		[3144]				= "BilgewaterCutlass",
+		[3188]				= "ItemBlackfireTorch",
+		[3153]				= "ItemSwordOfFeastAndFamine",
+		[3405]				= "TrinketSweeperLvl1",
+		[3411]				= "TrinketOrbLvl1",
+		[3166]				= "TrinketTotemLvl1",
+		[3450]				= "OdinTrinketRevive",
+		[2041]				= "ItemCrystalFlask",
+		[2054]				= "ItemKingPoroSnack",
+		[2138]				= "ElixirOfIron",
+		[2137]				= "ElixirOfRuin",
+		[2139]				= "ElixirOfSorcery",
+		[2140]				= "ElixirOfWrath",
+		[3184]				= "OdinEntropicClaymore",
+		[2050]				= "ItemMiniWard",
+		[3401]				= "HealthBomb",
+		[3363]				= "TrinketOrbLvl3",
+		[3092]				= "ItemGlacialSpikeCast",
+		[3460]				= "AscWarp",
+		[3361]				= "TrinketTotemLvl3",
+		[3362]				= "TrinketTotemLvl4",
+		[3159]				= "HextechSweeper",
+		[2051]				= "ItemHorn",
+		[3146]				= "HextechGunblade",
+		[3187]				= "HextechSweeper",
+		[3190]				= "IronStylus",
+		[2004]				= "FlaskOfCrystalWater",
+		[3139]				= "ItemMercurial",
+		[3222]				= "ItemMorellosBane",
+		[3042]				= "Muramana",
+		[3043]				= "Muramana",
+		[3180]				= "OdynsVeil",
+		[3056]				= "ItemFaithShaker",
+		[2047]				= "OracleExtractSight",
+		[3364]				= "TrinketSweeperLvl3",
+		[2052]				= "ItemPoroSnack",
+		[3140]				= "QuicksilverSash",
+		[3143]				= "RanduinsOmen",
+		[3074]				= "ItemTiamatCleave",
+		[3800]				= "ItemRighteousGlory",
+		[2045]				= "ItemGhostWard",
+		[3342]				= "TrinketOrbLvl1",
+		[3040]				= "ItemSeraphsEmbrace",
+		[3048]				= "ItemSeraphsEmbrace",
+		[2049]				= "ItemGhostWard",
+		[3345]				= "OdinTrinketRevive",
+		[2044]				= "SightWard",
+		[3341]				= "TrinketSweeperLvl1",
+		[3069]				= "shurelyascrest",
+		[3599]				= "KalistaPSpellCast",
+		[3185]				= "HextechSweeper",
+		[3077]				= "ItemTiamatCleave",
+		[2009]				= "ItemMiniRegenPotion",
+		[2010]				= "ItemMiniRegenPotion",
+		[3023]				= "ItemWraithCollar",
+		[3290]				= "ItemWraithCollar",
+		[2043]				= "VisionWard",
+		[3340]				= "TrinketTotemLvl1",
+		[3090]				= "ZhonyasHourglass",
+		[3154]				= "wrigglelantern",
+		[3142]				= "YoumusBlade",
+		[3157]				= "ZhonyasHourglass",
+		[3512]				= "ItemVoidGate",
+		[3131]				= "ItemSoTD",
+		[3137]				= "ItemDervishBlade",
+		[3352]				= "RelicSpotter",
+		[3350]				= "TrinketTotemLvl2",
+	}
+
+	___GetInventorySlotItem	= rawget(_G, "GetInventorySlotItem")
+	_G.GetInventorySlotItem	= GetSlotItem
+	
 	priorityTable = {
 			AP = {
 				"Annie", "Ahri", "Akali", "Anivia", "Annie", "Brand", "Cassiopeia", "Diana", "Evelynn", "FiddleSticks", "Fizz", "Gragas", "Heimerdinger", "Karthus",
@@ -490,6 +565,23 @@ function Variables()
 			elseif JungleMobNames[object.name] then
 				JungleMobs[#JungleMobs+1] = object
 			end
+		end
+	end
+end
+
+function GetSlotItem(id, unit)
+	unit = unit or myHero
+
+	if (not ItemNames[id]) then
+		return ___GetInventorySlotItem(id, unit)
+	end
+
+	local name	= ItemNames[id]
+	
+	for slot = ITEM_1, ITEM_7 do
+		local item = unit:GetSpellData(slot).name
+		if ((#item > 0) and (item:lower() == name:lower())) then
+			return slot
 		end
 	end
 end
