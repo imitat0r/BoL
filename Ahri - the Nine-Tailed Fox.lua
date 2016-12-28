240dbb4a11a034899bc276c6a43ecccfa9cbf3b4
Mateusz Borzęcki <DraconisBoL@users.noreply.github.com>
2016-12-28 21:53:12 +0100

    Update Ahri - the Nine-Tailed Fox.lua

--- Ahri - the Nine-Tailed Fox.lua
+++ Ahri - the Nine-Tailed Fox.lua
@@ -1,18 +1,17 @@
-local version = "1.4"
+local version = "1.5"
 
 --[[
 	Ahri - the Nine-Tailed Fox
 		Author: Draconis
-		Version: 1.4
-		Copyright 2014
+		Version: 1.5
+		Copyright 2016
 			
 	Dependency: Standalone
 --]]
 
 if myHero.charName ~= "Ahri" then return end
 
-require 'SxOrbwalk'
-require 'VPrediction'
+require 'HPrediction'
 
 ------------------------------------------------------
 --			 Callbacks				
@@ -22,7 +21,6 @@ function OnLoad()
 	print("<b><font color=\"#6699FF\">Ahri - the Nine-Tailed Fox:</font></b> <font color=\"#FFFFFF\">Good luck and have fun!</font>")
 	Variables()
 	Menu()
-	PriorityOnLoad()
 end
 
 function OnTick()
@@ -87,10 +85,6 @@ end
 
 function Combo(unit)
 	if ValidTarget(unit) and unit ~= nil and unit.type == myHero.type then
-		if Settings.combo.comboItems then
-			return
-		end
-		
 		if Settings.combo.comboMode == 1 then
 			if Settings.combo.useR then CastR(unit) end
 			CastE(unit)
@@ -133,14 +127,16 @@ end
 
 function JungleClear()
 	if Settings.jungle.jungleKey then
-		local JungleMob = GetJungleMob()
+		jungleMinions:update()
 		
-		if JungleMob ~= nil then
-			if Settings.jungle.jungleQ and GetDistance(JungleMob) <= SkillQ.range and SkillQ.ready then
-				CastSpell(_Q, JungleMob.x, JungleMob.z)
-			end
-			if Settings.jungle.jungleW and GetDistance(JungleMob) <= SkillW.range and SkillW.ready then
-				CastSpell(_W)
+		for i, JungleMob in pairs(jungleMinions.objects) do
+			if JungleMob ~= nil and JungleMob.health > 1 then
+				if Settings.jungle.jungleQ and GetDistance(JungleMob) <= SkillQ.range and SkillQ.ready then
+					CastSpell(_Q, JungleMob.x, JungleMob.z)
+				end
+				if Settings.jungle.jungleW and GetDistance(JungleMob) <= SkillW.range and SkillW.ready then
+					CastSpell(_W)
+				end
 			end
 		end
 	end
@@ -148,9 +144,9 @@ end
 
 function CastQ(unit)	
 	if unit ~= nil and GetDistance(unit) <= SkillQ.range and SkillQ.ready then
-		local CastPosition,  HitChance,  Position = VP:GetLineCastPosition(unit, SkillQ.delay, SkillQ.width, SkillQ.range, SkillQ.speed, myHero)
-					
-		if HitChance >= 2 then
+		local CastPosition,  HitChance = HPred:GetPredict(HPred.Presets["Ahri"]["Q"], unit, myHero)
+
+		if CastPosition and HitChance >= 1.6 then
 			CastSpell(_Q, CastPosition.x, CastPosition.z)
 		end
 	end
@@ -158,9 +154,9 @@ end
 
 function CastE(unit)
 	if unit ~= nil and GetDistance(unit) <= SkillE.range and SkillE.ready then
-		local CastPosition,  HitChance,  Position = VP:GetLineCastPosition(unit, SkillE.delay, SkillE.width, SkillE.range, SkillE.speed, myHero, true)
+		local CastPosition,  HitChance = HPred:GetPredict(HPred.Presets["Ahri"]["E"], unit, myHero)
 			
-		if HitChance >= 2 then
+		if CastPosition and HitChance >= 1.6 then
 			CastSpell(_E, CastPosition.x, CastPosition.z)
 		end
 	end
@@ -263,8 +259,6 @@ function Checks()
 	TargetSelector:update()
 	Target = GetCustomTarget()
 	
-	SxOrb:ForceTarget(Target)
-	
 	if Settings.drawing.lfc.lfc then _G.DrawCircle = DrawCircle2 else _G.DrawCircle = _G.oldDrawCircle end
 	
 	if Settings.combo.comboSwitch and Settings.combo.comboMode == 1 then
@@ -290,7 +284,6 @@ function Menu()
 		Settings.combo:addParam("comboSwitch", "Combo Switch", SCRIPT_PARAM_ONKEYTOGGLE, false, GetKey("A"))
 		Settings.combo:addParam("comboMode", "Combo Mode", SCRIPT_PARAM_LIST, 1, { "REQW", "EQW"})
 		Settings.combo:addParam("useR", "Use "..SkillR.name.." (R) in Combo", SCRIPT_PARAM_LIST, 1, { "To mouse", "Toward enemy", "Don't use"})
-		Settings.combo:addParam("comboItems", "Use Items in Combo", SCRIPT_PARAM_ONOFF, true)
 		Settings.combo:permaShow("comboKey")
 		Settings.combo:permaShow("comboMode")
 	
@@ -340,7 +333,11 @@ function Menu()
 			Settings.drawing.lfc:addParam("Width", "Width", 4, 1, 1, 10, 0)
 	
 	Settings:addSubMenu("["..myHero.charName.."] - Orbwalking Settings", "Orbwalking")
-		SxOrb:LoadToMenu(Settings.Orbwalking)
+		if _G.Reborn_Loaded ~= nil then
+			Settings.Orbwalking:addParam("Info", "Default orbwalker disabled", SCRIPT_PARAM_INFO, "")
+		else
+			NebelwolfisOrbWalkerClass(Settings.Orbwalking)
+		end
 	
 	TargetSelector = TargetSelector(TARGET_LESS_CAST, SkillE.range, DAMAGE_MAGIC, true)
 	TargetSelector.name = "Ahri"
@@ -352,11 +349,16 @@ function Variables()
 	SkillW = { name = "Fox-Fire", range = 800, delay = nil, speed = nil, width = nil, ready = false }
 	SkillE = { name = "Charm", range = 975, delay = 0.25, speed = 1500, width = 100, ready = false }
 	SkillR = { name = "Spirit Rush", range = 550, delay = nil, speed = nil, width = nil, ready = false }
-	Ignite = { name = "summonerdot", range = 600, slot = nil }
+	Ignite = { name = "SummonerDot", range = 600, slot = nil }
 	
-	enemyMinions = minionManager(MINION_ENEMY, SkillE.range, myHero, MINION_SORT_HEALTH_ASC)
+	enemyMinions = minionManager(MINION_ENEMY, SkillQ.range, myHero, MINION_SORT_HEALTH_ASC)
+	jungleMinions = minionManager(MINION_JUNGLE, SkillQ.range, myHero, MINION_SORT_MAXHEALTH_DEC)
 	
-	VP = VPrediction()
+	HPred = HPrediction()
+	
+	if _G.Reborn_Loaded == nil then
+		require("Nebelwolfi's Orb Walker")
+	end
 	
 	JungleMobs = {}
 	JungleFocusMobs = {}
@@ -369,46 +371,6 @@ function Variables()
 	
 	_G.oldDrawCircle = rawget(_G, 'DrawCircle')
 	_G.DrawCircle = DrawCircle2	
-	
-	priorityTable = {
-			AP = {
-				"Annie", "Ahri", "Akali", "Anivia", "Annie", "Brand", "Cassiopeia", "Diana", "Evelynn", "FiddleSticks", "Fizz", "Gragas", "Heimerdinger", "Karthus",
-				"Kassadin", "Katarina", "Kayle", "Kennen", "Leblanc", "Lissandra", "Lux", "Malzahar", "Mordekaiser", "Morgana", "Nidalee", "Orianna",
-				"Ryze", "Sion", "Swain", "Syndra", "Teemo", "TwistedFate", "Veigar", "Viktor", "Vladimir", "Xerath", "Ziggs", "Zyra", "Velkoz"
-			},
-			
-			Support = {
-				"Alistar", "Blitzcrank", "Janna", "Karma", "Leona", "Lulu", "Nami", "Nunu", "Sona", "Soraka", "Taric", "Thresh", "Zilean", "Braum"
-			},
-			
-			Tank = {
-				"Amumu", "Chogath", "DrMundo", "Galio", "Hecarim", "Malphite", "Maokai", "Nasus", "Rammus", "Sejuani", "Nautilus", "Shen", "Singed", "Skarner", "Volibear",
-				"Warwick", "Yorick", "Zac"
-			},
-			
-			AD_Carry = {
-				"Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Graves", "Jayce", "Jinx", "KogMaw", "Lucian", "MasterYi", "MissFortune", "Pantheon", "Quinn", "Shaco", "Sivir",
-				"Talon","Tryndamere", "Tristana", "Twitch", "Urgot", "Varus", "Vayne", "Yasuo", "Zed"
-			},
-			
-			Bruiser = {
-				"Aatrox", "Darius", "Elise", "Fiora", "Gangplank", "Garen", "Irelia", "JarvanIV", "Jax", "Khazix", "LeeSin", "Nocturne", "Olaf", "Poppy",
-				"Renekton", "Rengar", "Riven", "Rumble", "Shyvana", "Trundle", "Udyr", "Vi", "MonkeyKing", "XinZhao"
-			}
-	}
-
-	Items = {
-		BRK = { id = 3153, range = 450, reqTarget = true, slot = nil },
-		BWC = { id = 3144, range = 400, reqTarget = true, slot = nil },
-		DFG = { id = 3128, range = 750, reqTarget = true, slot = nil },
-		HGB = { id = 3146, range = 400, reqTarget = true, slot = nil },
-		RSH = { id = 3074, range = 350, reqTarget = false, slot = nil },
-		STD = { id = 3131, range = 350, reqTarget = false, slot = nil },
-		TMT = { id = 3077, range = 350, reqTarget = false, slot = nil },
-		YGB = { id = 3142, range = 350, reqTarget = false, slot = nil },
-		BFT = { id = 3188, range = 750, reqTarget = true, slot = nil },
-		RND = { id = 3143, range = 275, reqTarget = false, slot = nil }
-	}
 	
 	if not TwistedTreeline then
 		JungleMobNames = { 
@@ -488,106 +450,15 @@ function Variables()
 	end
 end
 
-function SetPriority(table, hero, priority)
-	for i=1, #table, 1 do
-		if hero.charName:find(table[i]) ~= nil then
-			TS_SetHeroPriority(priority, hero.charName)
-		end
-	end
-end
- 
-function arrangePrioritys()
-		for i, enemy in ipairs(GetEnemyHeroes()) do
-		SetPriority(priorityTable.AD_Carry, enemy, 1)
-		SetPriority(priorityTable.AP,	   enemy, 2)
-		SetPriority(priorityTable.Support,  enemy, 3)
-		SetPriority(priorityTable.Bruiser,  enemy, 4)
-		SetPriority(priorityTable.Tank,	 enemy, 5)
-		end
-end
-
-function arrangePrioritysTT()
-        for i, enemy in ipairs(GetEnemyHeroes()) do
-		SetPriority(priorityTable.AD_Carry, enemy, 1)
-		SetPriority(priorityTable.AP,       enemy, 1)
-		SetPriority(priorityTable.Support,  enemy, 2)
-		SetPriority(priorityTable.Bruiser,  enemy, 2)
-		SetPriority(priorityTable.Tank,     enemy, 3)
-        end
-end
-
-function UseItems(unit)
-	if unit ~= nil then
-		for _, item in pairs(Items) do
-			item.slot = GetInventorySlotItem(item.id)
-			if item.slot ~= nil then
-				if item.reqTarget and GetDistance(unit) < item.range then
-					CastSpell(item.slot, unit)
-				elseif not item.reqTarget then
-					if (GetDistance(unit) - getHitBoxRadius(myHero) - getHitBoxRadius(unit)) < 50 then
-						CastSpell(item.slot)
-					end
-				end
-			end
-		end
-	end
-end
-
-function getHitBoxRadius(target)
-	return GetDistance(target.minBBox, target.maxBBox)/2
-end
-
-function PriorityOnLoad()
-	if heroManager.iCount < 10 or (TwistedTreeline and heroManager.iCount < 6) then
-		print("<b><font color=\"#6699FF\">Ahri - the Nine-Tailed Fox:</font></b> <font color=\"#FFFFFF\">Too few champions to arrange priority.</font>")
-	elseif heroManager.iCount == 6 then
-		arrangePrioritysTT()
-    else
-		arrangePrioritys()
-	end
-end
-
-function GetJungleMob()
-	for _, Mob in pairs(JungleFocusMobs) do
-		if ValidTarget(Mob, SkillQ.range) then return Mob end
-	end
-	for _, Mob in pairs(JungleMobs) do
-		if ValidTarget(Mob, SkillQ.range) then return Mob end
-	end
-end
-
-function OnCreateObj(obj)
-	if obj.valid then
-		if FocusJungleNames[obj.name] then
-			JungleFocusMobs[#JungleFocusMobs+1] = obj
-		elseif JungleMobNames[obj.name] then
-			JungleMobs[#JungleMobs+1] = obj
-		end
-	end
-end
-
-function OnDeleteObj(obj)
-	for i, Mob in pairs(JungleMobs) do
-		if obj.name == Mob.name then
-			table.remove(JungleMobs, i)
-		end
-	end
-	for i, Mob in pairs(JungleFocusMobs) do
-		if obj.name == Mob.name then
-			table.remove(JungleFocusMobs, i)
-		end
-	end
-end
-
 function TrueRange()
 	return myHero.range + GetDistance(myHero, myHero.minBBox)
 end
 
 -- Trees
 function GetCustomTarget()
- 	TargetSelector:update() 	
+	TargetSelector:update() 	
 	if _G.MMA_Target and _G.MMA_Target.type == myHero.type then return _G.MMA_Target end
-	if _G.AutoCarry and _G.AutoCarry.Crosshair and _G.AutoCarry.Attack_Crosshair and _G.AutoCarry.Attack_Crosshair.target and _G.AutoCarry.Attack_Crosshair.target.type == myHero.type then return _G.AutoCarry.Attack_Crosshair.target end
+	--if _G.AutoCarry and _G.AutoCarry.Crosshair and _G.AutoCarry.Attack_Crosshair and _G.AutoCarry.Attack_Crosshair.target and _G.AutoCarry.Attack_Crosshair.target.type == myHero.type then return _G.AutoCarry.Attack_Crosshair.target end
 	return TargetSelector.target
 end
 
